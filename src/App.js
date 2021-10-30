3124189dfda4d57678d6639632a3603717f5eec9
mahmudimran <mahmudimran203@gmail.com>
2021-10-30 19:03:38 +0600

    sign in auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, GithubAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+const app = initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({})
+  const GoogleProvider = new GoogleAuthProvider();
+  const GitProvider = new GithubAuthProvider();
+  const FbProvider = new FacebookAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        setUser(user)
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        console.log(errorCode, errorMessage, email)
+      });
+  }
+
+  const handleGitSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, GitProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        setUser(user)
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      });
+  }
+  const handleFbSignIn = () => {
+
+    const auth = getAuth();
+    signInWithPopup(auth, FbProvider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+        // ...
+        setUser(user)
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode,errorMessage )
+      });
+
+  }
+  const { displayName, email, photoURL } = user
+  const styleButons = {
+    padding:'10px',
+    margin:'10px',
+    backgroundColor:'#FA3E3E',
+    border:'none',
+    color:'white',
+    fontSize:'17px',
+    cursor:'pointer'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:'center', marginTop:'30px'}}>
+      <button style={styleButons} onClick={handleGoogleSignIn}>Google Sign In </button>
+      <button style={styleButons} onClick={handleGitSignIn}>Github sign in</button>
+      <button style={styleButons} onClick={handleFbSignIn}>Facebook Sign In</button>
+      <div>
+        <h3>{displayName}</h3>
+        <p>{email}</p>
+        <img src={photoURL} alt="" />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQ890Xz4ve5TOcMukyDV91Np_MJlry_wA",
+    authDomain: "fir-auth-recap-4f483.firebaseapp.com",
+    projectId: "fir-auth-recap-4f483",
+    storageBucket: "fir-auth-recap-4f483.appspot.com",
+    messagingSenderId: "773394294561",
+    appId: "1:773394294561:web:cb1ccf38fc9fd3515cc060"
+  };
+
+  export default firebaseConfig;
